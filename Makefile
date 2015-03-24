7ac190eec48467c6be2f606ead2f7a8d54e2e0b9
Viktor <viteg34@yandex.ru>
2015-03-24 22:10:16 +0300

    task 3 done

--- Makefile
+++ Makefile
@@ -2,3 +2,4 @@ all:
 	$(MAKE) -C lib
 	$(MAKE) -C cat
 	$(MAKE) -C revwords
+	$(MAKE) -C filter
--- filter/filter.c
+++ filter/filter.c
@@ -7,10 +7,65 @@
 
 const size_t SIZE = 4096;
 
-int main() {
-    char* args[] = {"ls", ".f", NULL};
-    int res = spawn("ls", args);
-    printf("res: %d", res);
+int main(int argc, char* argv[]) {
+    char buf[SIZE];
+    char word[SIZE + 1];
+    int r, w;
+    int index = 0;
+
+    for(int i = 0; i < argc; ++i)
+    {
+        argv[i] = argv[i + 1];
+    }
+
+    while(1)
+    {
+        r = read_until(STDIN_FILENO, buf, SIZE, '\n');
+        if (r == 0)
+            break;
+        if (r < 0) {
+            perror("read");
+            exit(1);
+        }
+        for(int i = 0; i < r; ++i)
+        {
+            if(((char*)buf)[i] == '\n')
+            {
+                word[index] = 0;
+                argv[argc - 1] = word;
+                int sp_res = spawn(argv[0], argv);
+                if(sp_res == 0)
+                {
+                    word[index++] = '\n';
+                    w = write_(STDOUT_FILENO, word, index);
+                    if(w < 0)
+                    {
+                        perror("write");
+                        exit(1);
+                    }
+                }
+                index = 0;
+            } else {
+                word[index++] = buf[i];
+            }
+        }
+    }
+    if(index > 0)
+    {
+        word[index] = 0;
+        argv[argc - 1] = word;
+        int sp_res = spawn(argv[0], argv);
+        if(sp_res == 0)
+        {
+            word[index++] = '\n';
+            w = write_(STDOUT_FILENO, word, index);
+            if(w < 0)
+            {
+                perror("write");
+                exit(1);
+            }
+        }
+    }
 
     return 0;
 }
